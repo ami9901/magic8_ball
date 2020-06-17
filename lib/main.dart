172c8d51d7ff6f15fdca7330226cff88b6cc604f
AMISHA NARSINGANI <amishanarsingani@gmail.com>
2020-06-17 23:56:06 +0530

    completed step 2

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,19 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(
-      MaterialApp(
-        home: null,
+void main() => runApp(BallPage());
+
+class BallPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blue,
+        appBar: AppBar(
+          title: Text('Ask Me Anything'),
+          backgroundColor: Colors.blue.shade900,
+        ),
+        body: Container(),
       ),
     );
+  }
+}

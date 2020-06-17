7718d1eeea07edbbed22e5b1b5745651506883db
AMISHA NARSINGANI <amishanarsingani@gmail.com>
2020-06-18 00:09:56 +0530

    completed step 3

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,36 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(BallPage());
+void main() => runApp(
+      MaterialApp(
+        home: BallPage(),
+      ),
+    );
 
 class BallPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.blue,
-        appBar: AppBar(
-          title: Text('Ask Me Anything'),
-          backgroundColor: Colors.blue.shade900,
-        ),
-        body: Container(),
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      appBar: AppBar(
+        title: Text('Ask Me Anything'),
+        backgroundColor: Colors.blue.shade900,
+      ),
+      body: Ball(),
+    );
+  }
+}
+
+class Ball extends StatefulWidget {
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Image.asset('images/ball1.png'),
       ),
     );
   }
